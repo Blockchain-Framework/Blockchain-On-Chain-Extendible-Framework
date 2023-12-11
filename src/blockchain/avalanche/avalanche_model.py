e19dd38380b9dd81bd38445223e623068c95de15
Kasun Abegunawardhana <75435489+KasunAb@users.noreply.github.com>
2023-12-11 10:57:09 +0530

    Delete unnecessary files and code

--- src/blockchain/avalanche/avalanche_model.py
+++
@@ -1,9 +0,0 @@
-# avalanche_model.py
-from src.models.general_blockchain_model import GeneralBlockchainModel
-
-class Avalanche_X_Model(GeneralBlockchainModel):
-    def __init__(self, txHash, blockHash, timestamp, value, txType, memo, chainFormat):
-        super().__init__(txHash, blockHash, timestamp, value)  # Initialize parent class
-        self.txType = txType
-        self.memo = memo
-        self.chainFormat = chainFormat
--- src/models/general_blockchain_model.py
+++
@@ -1,9 +0,0 @@
-# general_blockchain_model.py
-
-class GeneralBlockchainModel:
-    def __init__(self, txHash, blockHash, timestamp, value):
-        self.txHash = txHash
-        self.blockHash = blockHash
-        self.timestamp = timestamp
-        self.value = value 
-        
