0632cdfa63074f16cd02ca56ee172371c82aa236
Kasun Abegunawardhana <75435489+KasunAb@users.noreply.github.com>
2023-12-14 11:09:03 +0530

    Refactor Avalanche models, extraction and remove unused files

--- src/blockchain/avalanche/avalanche_data_extraction.py
+++ src/blockchain/avalanche/avalanche_data_extraction.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from src.utils.http_utils import fetch_transactions
 from src.blockchain.avalanche.avalanche_model import Avalanche_X_Model, Avalanche_C_Model, Avalanche_P_Model
 
-def extract_x_chain_data(last_block):
+def extract_x_chain_data(last_timestamp):
     page_token = None
 
     params = {
@@ -27,29 +27,29 @@ def extract_x_chain_data(last_block):
         transactions = res_data.get('transactions', [])
 
         for tx in transactions:
-          block_height = tx.get("blockHeight")
-          if block_height<= last_block:
-            run = False
-            break
+            timestamp = int(tx.get("timestamp"))
+            if timestamp <= last_timestamp:
+                run = False
+                break
 
-          avalanche_tx = Avalanche_X_Model(
-            txHash=tx.get("txHash"),
-            blockHash=tx.get("blockHash"),
-            blockHeight = block_height,
-            timestamp=tx.get("timestamp"),
-            value=calculate_x_transaction_value(tx),
-            memo=tx.get("memo"),
-            chainFormat=tx.get("chainFormat"),
-            txType=tx.get("txType") 
-          )
+            avalanche_tx = Avalanche_X_Model(
+                txHash=tx.get("txHash"),
+                blockHash=tx.get("blockHash"),
+                blockHeight = tx.get("blockHeight"),
+                timestamp=timestamp,
+                value=calculate_x_transaction_value(tx),
+                memo=tx.get("memo"),
+                chainFormat=tx.get("chainFormat"),
+                txType=tx.get("txType") 
+            )
+                
+            page_token = res_data.get('nextPageToken')
             
-          page_token = res_data.get('nextPageToken')
-          
-          data.append(avalanche_tx.__dict__)
+            data.append(avalanche_tx.__dict__)
         
     return pd.DataFrame(data)
 
-def extract_c_chain_data(last_block):
+def extract_c_chain_data(last_timestamp):
     page_token = None
     
     params = {
@@ -63,40 +63,84 @@ def extract_c_chain_data(last_block):
     run = True
 
     while run:
-      if page_token:
-          params["pageToken"] = page_token
-    
-      res_data = fetch_transactions(url, params)
-      transactions = res_data.get('transactions', [])
+        if page_token:
+            params["pageToken"] = page_token
+        
+        res_data = fetch_transactions(url, params)
+        transactions = res_data.get('transactions', [])
 
-      for tx in transactions:
-          block_height = tx.get("blockHeight")
-                    
-          if block_height<= last_block:
-            run = False
-            break
+        for tx in transactions:
+            
+            timestamp=int(tx.get("timestamp"))          
+            if timestamp <= last_timestamp:
+                run = False
+                break
 
-          total_input_value, total_output_value = calculate_c_chain_transaction_value(tx)
-          
-          avalanche_tx = Avalanche_C_Model(
-              txHash=tx.get("txHash"),
-              blockHash=tx.get("blockHash"),
-              blockHeight=block_height,
-              txType=tx.get("txType"),
-              timestamp=tx.get("timestamp"),
-              sourceChain=tx.get("sourceChain"),
-              destinationChain=tx.get("destinationChain"),
-              memo=tx.get("memo"),
-              total_input_value = total_input_value,
-              total_output_value = total_output_value
-          )
+            total_input_value, total_output_value = calculate_c_chain_transaction_value(tx)
+            
+            avalanche_tx = Avalanche_C_Model(
+                txHash=tx.get("txHash"),
+                blockHash=tx.get("blockHash"),
+                blockHeight=tx.get("blockHeight"),
+                txType=tx.get("txType"),
+                timestamp=timestamp,
+                sourceChain=tx.get("sourceChain"),
+                destinationChain=tx.get("destinationChain"),
+                memo=tx.get("memo"),
+                total_input_value = total_input_value,
+                total_output_value = total_output_value
+            )
 
-          page_token = res_data.get('nextPageToken')
-          
-          data.append(avalanche_tx.__dict__)
+            page_token = res_data.get('nextPageToken')
+            
+            data.append(avalanche_tx.__dict__)
     
     return pd.DataFrame(data)
 
+def extract_p_chain_data(last_timestamp):
+    page_token = None
+    params = {"pageSize": 100}
+    url = "https://glacier-api.avax.network/v1/networks/mainnet/blockchains/p-chain/transactions"
+    data = []
+    run = True
+
+    while run:
+        if page_token:
+            params["pageToken"] = page_token
+        
+        res_data = fetch_transactions(url, params)
+        transactions = res_data.get('transactions', [])
+
+        for tx in transactions:
+            timestamp = int(tx.get("blockTimestamp"))
+            if timestamp <= last_timestamp:
+                run = False
+                break
+
+            amountStaked = calculate_p_transaction_value(tx.get("amountStaked", []))
+            amountBurned = calculate_p_transaction_value(tx.get("amountBurned", []))
+
+            p_tx = Avalanche_P_Model(
+                txHash=tx.get("txHash"),
+                txType=tx.get("txType"),
+                blockTimestamp=timestamp,
+                blockNumber=tx.get("blockNumber"),
+                blockHash=tx.get("blockHash"),
+                memo=tx.get("memo"),
+                nodeId=tx.get("nodeId"),
+                subnetId=tx.get("subnetId"),
+                amountStaked=amountStaked,
+                amountBurned=amountBurned
+            )
+            data.append(p_tx.__dict__)
+
+        page_token = res_data.get('nextPageToken')
+
+    return pd.DataFrame(data)
+
+def calculate_p_transaction_value(amounts):
+    total_value = sum(int(asset['amount']) for asset in amounts) / 10**9  # Convert to AVAX
+    return total_value
 
 def calculate_x_transaction_value(transaction):
     #TO DO : Handle whwn asset not avalanche
@@ -129,10 +173,12 @@ def calculate_c_chain_transaction_value(transaction):
 
     return total_input_value, total_output_value
 
-def extract_avalanche_data(last_x_block, last_c_block):
-    x_chain_data = extract_x_chain_data(last_x_block)
-    c_chain_data = extract_c_chain_data(last_c_block)
-    print(type(c_chain_data))
+
+def extract_avalanche_data(last_x_time, last_c_time,last_p_time):
+    x_chain_data = extract_x_chain_data(last_x_time)
+    c_chain_data = extract_c_chain_data(last_c_time)
+    p_chain_data = extract_p_chain_data(last_p_time)
+
     
     # p_chain_data = extract_p_chain_data(last_block)
-    return x_chain_data, c_chain_data
+    return x_chain_data, c_chain_data, p_chain_data
--- src/services/data_storage_service.py
+++ src/services/data_storage_service.py
@@ -1,13 +1,8 @@
 import pandas as pd
 import psycopg2
+import numpy
 from sqlalchemy import create_engine
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MY_ENV_VAR = os.getenv('MY_ENV_VAR')
 def store_data(dataframe, file_path, data_base, db_connection_string):
     # TO DO : Store date vise
     # Store as .tsv.gz
@@ -18,8 +13,52 @@ def store_data(dataframe, file_path, data_base, db_connection_string):
     print("Database created successfully")
     # Store to PostgreSQL
     engine = create_engine(db_connection_string)
+    
+    # TO DO:if_exists='append'
     dataframe.to_sql(data_base, engine, if_exists='replace', index=False)
+    
+    
+def get_last_transaction_data(db_connection_string, blockchain):
 
+    # Connect to PostgreSQL Server
+    conn = psycopg2.connect(db_connection_string)
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    # Create database if it does not exist
+    cursor.execute(f"SELECT MAX(timestamp) FROM workflow_meta_table WHERE blockchain = '{blockchain}';")
+
+    # Fetch all the rows and print them
+    rows = cursor.fetchall()
+    if rows:
+        cursor.close()
+        conn.close()
+        return rows[0][0]
+    else:
+        return None
+
+import psycopg2
+
+def set_last_transaction_data(db_connection_string, blockchain, timestamp, task=""):
+    # Convert numpy.int64 to native Python int
+    if isinstance(timestamp, numpy.int64):
+        timestamp = int(timestamp)
+
+    conn = psycopg2.connect(db_connection_string)
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    # Use parameterized query to avoid SQL injection
+    query = "INSERT INTO workflow_meta_table (timestamp, task, blockchain) VALUES (%s, %s, %s);"
+    
+    # Execute the query with parameters
+    cursor.execute(query, (timestamp, task, blockchain))
+    
+    cursor.close()
+    conn.close()
+
+
+    
 def create_database_and_table(db_connection_string):
     # Extracting the database name from the connection string
     db_name = db_connection_string.split('/')[-1]
@@ -41,7 +80,7 @@ def create_database_and_table(db_connection_string):
     cursor = conn.cursor()
 
     # Create table if it does not exist
-    # TO DO : work flow meta table (task, blockchain date)
+    # TO DO : work flow meta table (task, blockchain, date)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS x_avalanche_data (
             txHash TEXT,
@@ -68,6 +107,11 @@ def create_database_and_table(db_connection_string):
             total_output_value NUMERIC,
             PRIMARY KEY (txHash)
         );
+        CREATE TABLE IF NOT EXISTS workflow_meta_table (
+            timestamp BIGINT,
+            task VARCHAR(100),
+            blockchain VARCHAR(100)
+            );
     """)
 
     # Close connection
