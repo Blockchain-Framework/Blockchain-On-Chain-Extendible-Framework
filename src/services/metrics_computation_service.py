070553766e4d1a1ec6e606ac744e6038fe8e36ca
Kasun Abegunawardhana <75435489+KasunAb@users.noreply.github.com>
2023-12-14 11:22:15 +0530

    Add metrics computation service file

---
+++ src/services/metrics_computation_service.py
@@ -0,0 +1,26 @@
+# Daily Transaction Count
+
+# Daily Transaction Volume
+
+# Average Transaction Value
+
+# Active Addresses
+
+# Block Size
+
+# Network Hash Rate (for Proof-of-Work Blockchains)
+
+# Smart Contract Execution Metrics
+
+# Token Transfer Count (for Ethereum-based Tokens)
+
+# Token Transfer Volume (for Ethereum-based Tokens)
+
+# Token Holders (for Ethereum-based Tokens)
+
+# Token Price (for Ethereum-based Tokens)
+
+
+
+
+
