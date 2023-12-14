24590ff8f4bb928aa2ae1606dcdca557b401a5c3
Bimsara Bodaragama <bodaragamadb.office@gmail.com>
2023-12-15 02:51:00 +0530

    Mterics Calc - Phase 1

--- src/services/metrics_computation_service.py
+++ src/services/metrics_computation_service.py
@@ -3,6 +3,7 @@ import psycopg2
 import numpy as np
 from sqlalchemy import create_engine
 
+# (1) Daily Transaction Count
 def compute_transaction_count(dataframe, date, chain, db_connection_string, table_name = 'daily_transaction_count'):
 
     # Connect to PostgreSQL Server
@@ -28,25 +29,53 @@ def compute_transaction_count(dataframe, date, chain, db_connection_string, tabl
     conn.close()
     return transaction_count
 
-# Daily Transaction Volume
+# Average Transactions Per Block
+def compute_average_transactions_per_block(dataframe, date, chain, db_connection_string, table_name='average_transactions_per_block'):
+    conn = psycopg2.connect(db_connection_string)
+    conn.autocommit = True
+    cursor = conn.cursor()
 
-# Average Transaction Value
+    # Group by blockHash and calculate average transactions per block
+    avg_transactions_per_block = dataframe.groupby('blockHash').size().mean()
 
-# Active Addresses
+    # Create table and insert data
+    cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            date VARCHAR(100),
+            chain_name VARCHAR(255),
+            avg_transactions_per_block FLOAT
+        );
+        INSERT INTO {table_name} (date, chain_name, avg_transactions_per_block) VALUES (%s, %s, %s)""",
+                   (date, chain, avg_transactions_per_block))
 
-# Block Size
+    cursor.close()
+    conn.close()
+    return avg_transactions_per_block
 
-# Network Hash Rate (for Proof-of-Work Blockchains)
 
-# Smart Contract Execution Metrics
 
-# Token Transfer Count (for Ethereum-based Tokens)
 
-# Token Transfer Volume (for Ethereum-based Tokens)
+# (2) Daily Transaction Volume
 
-# Token Holders (for Ethereum-based Tokens)
+# (3) Average Transaction Value
 
-# Token Price (for Ethereum-based Tokens)
+# (4) Active Senders
+
+# (5) Active Addresses
+
+# (6) Block Size
+
+# (7) Network Hash Rate (for Proof-of-Work Blockchains)
+
+# (8) Smart Contract Execution Metrics
+
+# (9) Token Transfer Count (for Ethereum-based Tokens)
+
+# (10) Token Transfer Volume (for Ethereum-based Tokens)
+
+# (11) Token Holders (for Ethereum-based Tokens)
+
+# (12) Token Price (for Ethereum-based Tokens)
 
 
 
