111586d4edf4d28c7a1f69a81f07ddc317c8a76b
Bimsara Bodaragama <bodaragamadb.office@gmail.com>
2023-12-21 01:56:46 +0530

    Initializing API - Add Pagination to routes 3 - (In Data Pane)

--- src/api/routes/metric.py
+++ src/api/routes/metric.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 from models.metric import (DailyTransactionCount, AverageTransactionsPerBlock, TotalStakedAmount,
                            TotalBurnedAmount, AverageTransactionValue, LargeTransactionMonitoring,
                            CrossChainWhaleActivity)
-from models.response import Response
+from models.response import Response # Import the custom Response
+from utils.json_utils import jsonify  # Import the custom jsonify
 from datetime import datetime
 from sqlalchemy.exc import SQLAlchemyError
 
-# Create a blueprint for your metrics routes
 metrics_blueprint = Blueprint('metrics', __name__)
 
 def filter_by_date(model, query, start_date, end_date):
@@ -30,43 +30,37 @@ def validate_date(date_str):
 
 def get_paginated_data(model, query):
     """
-    Apply pagination to a database query.
+    Apply pagination to a database query and return the paginated query.
     """
-    # Retrieve pagination parameters from request
     page = request.args.get('page', 1, type=int)
     page_size = request.args.get('page_size', 10, type=int)
-
-    # Paginate the query
-    paginated_query = query.paginate(page, page_size, False)
-    return paginated_query.items
+    return query.paginate(page, page_size, False)
 
 def handle_metric_route(model):
     """
     Handle the route logic, including validation, querying, and response formatting.
     """
     try:
-        # Validate the date parameters
         start_date = validate_date(request.args.get('start_date'))
         end_date = validate_date(request.args.get('end_date'))
 
-        # Raise an error if date validation fails
         if start_date is None or end_date is None:
             raise ValueError("Invalid date format. Use YYYY-MM-DD.")
 
-        # Prepare and execute the query
         query = model.query
         query = filter_by_date(model, query, start_date, end_date)
-        items = get_paginated_data(model, query)
+        paginated_query = get_paginated_data(model, query)
+        items = paginated_query.items
 
-        # Create a successful response
         response = Response(True, [item.serialize() for item in items], len(items))
         return jsonify(response.to_dict()), 200
     except ValueError as ve:
-        # Return a 400 error for invalid date formats
         return jsonify(Response(False, error=str(ve)).to_dict()), 400
     except SQLAlchemyError:
-        # Return a 500 error for database operation issues
         return jsonify(Response(False, error="Database error").to_dict()), 500
+    except Exception as e:
+        # Generic catch for any other unexpected exceptions
+        return jsonify(Response(False, error=f"An unexpected error occurred: {str(e)}").to_dict()), 500
 
 # Define individual routes for each metric
 @metrics_blueprint.route('/daily_transaction_count')
@@ -96,4 +90,3 @@ def get_large_transaction_monitoring():
 @metrics_blueprint.route('/cross_chain_whale_activity')
 def get_cross_chain_whale_activity():
     return handle_metric_route(CrossChainWhaleActivity)
-
