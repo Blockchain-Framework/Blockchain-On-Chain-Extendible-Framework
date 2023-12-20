3c7dcd855ca45b1d7bdd027eb315c8690bbd60c1
Bimsara Bodaragama <bodaragamadb.office@gmail.com>
2023-12-21 01:16:10 +0530

    Initializing API - Add Pagination to routes - (In Data Pane)

--- src/api/routes/metric.py
+++ src/api/routes/metric.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, request
-from models.metric import (DailyTransactionCount, AverageTransactionsPerBlock, TotalStakedAmount, 
-                           TotalBurnedAmount, AverageTransactionValue, LargeTransactionMonitoring, 
+from models.metric import (DailyTransactionCount, AverageTransactionsPerBlock, TotalStakedAmount,
+                           TotalBurnedAmount, AverageTransactionValue, LargeTransactionMonitoring,
                            CrossChainWhaleActivity)
 from datetime import datetime
+from sqlalchemy.exc import SQLAlchemyError
 
 metrics_blueprint = Blueprint('metrics', __name__)
 
@@ -19,93 +20,64 @@ def validate_date(date_str):
     except ValueError:
         return None
 
-@metrics_blueprint.route('/daily_transaction_count')
-def get_daily_transaction_count():
+def get_paginated_data(model, query):
+    try:
+        # Pagination parameters
+        page = request.args.get('page', 1, type=int)
+        page_size = request.args.get('page_size', 10, type=int)
+
+        # Query with pagination
+        paginated_query = query.paginate(page, page_size, False)
+        items = paginated_query.items
+
+        # Successful response
+        return {
+            "status": True,
+            "data": [item.serialize() for item in items],
+            "page_size": len(items)
+        }
+    except SQLAlchemyError:
+        # Error in database operation
+        return {"status": False, "error": "Database error"}
+
+# Function to handle each metric route
+def handle_metric_route(model):
     start_date = validate_date(request.args.get('start_date'))
     end_date = validate_date(request.args.get('end_date'))
 
     if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
+        return jsonify({"status": False, "error": "Invalid date format. Use YYYY-MM-DD."}), 400
 
-    query = DailyTransactionCount.query
-    query = filter_by_date(DailyTransactionCount, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    query = model.query
+    query = filter_by_date(model, query, start_date, end_date)
+    response = get_paginated_data(model, query)
+    return jsonify(response), response.get("status", 500)
+
+# Define routes
+@metrics_blueprint.route('/daily_transaction_count')
+def get_daily_transaction_count():
+    return handle_metric_route(DailyTransactionCount)
 
 @metrics_blueprint.route('/average_transactions_per_block')
 def get_average_transactions_per_block():
-    start_date = validate_date(request.args.get('start_date'))
-    end_date = validate_date(request.args.get('end_date'))
-
-    if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-
-    query = AverageTransactionsPerBlock.query
-    query = filter_by_date(AverageTransactionsPerBlock, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    return handle_metric_route(AverageTransactionsPerBlock)
 
 @metrics_blueprint.route('/total_staked_amount')
 def get_total_staked_amount():
-    start_date = validate_date(request.args.get('start_date'))
-    end_date = validate_date(request.args.get('end_date'))
-
-    if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-
-    query = TotalStakedAmount.query
-    query = filter_by_date(TotalStakedAmount, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    return handle_metric_route(TotalStakedAmount)
 
 @metrics_blueprint.route('/total_burned_amount')
 def get_total_burned_amount():
-    start_date = validate_date(request.args.get('start_date'))
-    end_date = validate_date(request.args.get('end_date'))
-
-    if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-
-    query = TotalBurnedAmount.query
-    query = filter_by_date(TotalBurnedAmount, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    return handle_metric_route(TotalBurnedAmount)
 
 @metrics_blueprint.route('/average_transaction_value')
 def get_average_transaction_value():
-    start_date = validate_date(request.args.get('start_date'))
-    end_date = validate_date(request.args.get('end_date'))
-
-    if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-
-    query = AverageTransactionValue.query
-    query = filter_by_date(AverageTransactionValue, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    return handle_metric_route(AverageTransactionValue)
 
 @metrics_blueprint.route('/large_transaction_monitoring')
 def get_large_transaction_monitoring():
-    start_date = validate_date(request.args.get('start_date'))
-    end_date = validate_date(request.args.get('end_date'))
-
-    if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-
-    query = LargeTransactionMonitoring.query
-    query = filter_by_date(LargeTransactionMonitoring, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    return handle_metric_route(LargeTransactionMonitoring)
 
 @metrics_blueprint.route('/cross_chain_whale_activity')
 def get_cross_chain_whale_activity():
-    start_date = validate_date(request.args.get('start_date'))
-    end_date = validate_date(request.args.get('end_date'))
-
-    if start_date is None or end_date is None:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-
-    query = CrossChainWhaleActivity.query
-    query = filter_by_date(CrossChainWhaleActivity, query, start_date, end_date)
-    data = query.all()
-    return jsonify([item.serialize() for item in data])
+    return handle_metric_route(CrossChainWhaleActivity)
