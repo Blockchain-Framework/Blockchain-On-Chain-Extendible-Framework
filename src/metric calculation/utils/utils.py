beb005c9dc4118e7b5fd930ad4d0e18fd59e8dfd
Kasun Abegunawardhana <75435489+KasunAb@users.noreply.github.com>
2024-01-31 17:34:41 +0530

    Refactor log_workflow_status function

--- src/metric calculation/utils/utils.py
+++ src/metric calculation/utils/utils.py
@@ -1,6 +1,7 @@
 from utils.database_service import append_dataframe_to_sql
+import pandas as pd
 
-def log_workflow_status(chain, subchain, status, task, error, database_connection):
+def log_workflow_status(chain, subchain, status, task, error):
     data = {
         'chain': [chain],
         'subchain': [subchain],
@@ -9,4 +10,4 @@ def log_workflow_status(chain, subchain, status, task, error, database_connectio
         'error': [error if error else 'None']
     }
     df = pd.DataFrame(data)
-    append_dataframe_to_sql('workflow_meta_table', df, database_connection)
+    append_dataframe_to_sql('workflow_meta_table', df)
